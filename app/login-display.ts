1ffacb70dc0f81f52bc6959fdab20c072051d47e
philomagi <tooppoo56@gmail.com>
2022-07-23 04:14:21 +0900

    impl test servlet

---
+++ app/login-display.ts
@@ -0,0 +1,24 @@
+import { HttpServlet } from "@servlet/http-servlet"
+import { IncomingMessage, ServerResponse } from "node:http"
+import {getRequestDispatcher, RequestDispatcher} from "@servlet/dispatcher";
+
+export class LoginDisplayServlet extends HttpServlet {
+    doGet(req: IncomingMessage, res: ServerResponse) {
+        getRequestDispatcher(loginDisplayDispatcher, []).forward(req, res)
+    }
+}
+
+const loginDisplayDispatcher: RequestDispatcher = () => `
+<h1>Login</h1>
+<form>
+    <div>
+        ID: <input id="user_id">
+    </div>
+    <div>
+        パスワード: <input id="password" type="password">
+    </div>
+    <div>
+        <button type="submit">ログイン</button>
+    </div>
+</form>
+`
