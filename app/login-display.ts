342d7a37565c4294161615cb747a0f9b54e53385
philomagi <tooppoo56@gmail.com>
2022-07-23 23:26:18 +0900

    remove apps and views

--- app/login-display.ts
+++
@@ -1,9 +0,0 @@
-import { HttpServlet } from "@servlet/http-servlet"
-import {HttpRequest} from "@servlet/request";
-import {HttpResponse} from "@servlet/response";
-
-export class LoginDisplayServlet extends HttpServlet {
-    protected doGet(req: HttpRequest, res: HttpResponse): Promise<void> {
-        return req.getRequestDispatcher('login.ts#login').forward(req, res)
-    }
-}
